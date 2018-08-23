0987439aea0b9ce7c15373de5c4e5018b244f06d
xuliran33 <476883317@qq.com>
2018-08-23 11:14:38 +0800

    向上转型向下转型

--- ShuZudemo/src/com/xlr/extend/Test.java
+++ ShuZudemo/src/com/xlr/extend/Test.java
@@ -18,11 +18,26 @@ public class Test {
 		System.out.println("******************");
 		
 		// 父类引用指向子类对象
+		// 向上转型
+		// <父类型><引用变量名> = new <子类型>()
+		// 此时通过父类引用变量调用的方法是子类覆盖或继承父类的方法, 不是父类的方法
+		// 此时通过父类引用变量无法调用子类特有的方法
 		Pet penguin = new Penguin();
+		
 		penguin.setName("企鹅");
 		master.cure(penguin);
 		System.out.println(penguin.getHealth());
 		master.feed(penguin);
+		
+		// 向下转型, 类似强制类型转换
+		// <子类型> <引用变量名> = (<子类型>)<父类型的引用变量>
+		// 在向下转型的过程中, 如果没有转换为真实子类类型, 会出现类型转换异常java.lang.ClassCastException, 如在此处把penguin转换为dog, 编译可通过, 运行异常
+		// 避免错误 instanceOf
+		if (penguin instanceof Penguin) {
+			Penguin p = (Penguin) penguin;
+			p.setSex("Q妹");
+		}
+	
 		penguin.print();
 	}
 }
