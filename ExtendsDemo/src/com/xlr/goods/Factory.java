8a57c317ffb42077160f3dba93fefce47b2a75cd
xuliran <xuliran123@126.com>
2018-08-24 14:35:04 +0800

    绑定机制

--- ExtendsDemo/src/com/xlr/goods/Factory.java
+++ ExtendsDemo/src/com/xlr/goods/Factory.java
@@ -1,6 +1,8 @@
 package com.xlr.goods;
 // 工厂类: 根据用户提供的条件, 返回相应商品
-// 简单工厂模式
+// 简单工厂模式: 由工厂类根据客户传进来的具体参数, 决定生产某种产品
+// 工厂类  抽象产品类  具体产品类  客户
+// 抽象工厂模式
 public class Factory {
 	public static Goods getGood(String type) {
 		Goods goods = null;
--- ExtendsDemo/src/com/xlr/goods/Goods.java
+++ ExtendsDemo/src/com/xlr/goods/Goods.java
@@ -3,4 +3,8 @@ package com.xlr.goods;
 public abstract class Goods {
 	// 商品类: 打印商品价格
 	public abstract void printPrice();
+	
+	public static void m1() {
+	
+	}
 }
