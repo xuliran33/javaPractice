b4b1b77374d3c8052babce3dae4c7985c5edc795
xuliran33 <476883317@qq.com>
2018-08-28 16:05:17 +0800

    多重catch块

--- ExceptionDemo/src/Test3.java
+++ ExceptionDemo/src/Test3.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 // try -catch - finally, try语句块是必须的, catch, finally语句均可选, 但必须出现其一
 // finally中的代码不管是否捕捉到异常都执行
@@ -6,6 +7,14 @@ import java.util.Scanner;
 // try -catch - finally块中, finally块唯一不执行的情况是什么?
 
 // 使用场景: 程序中发生异常时, 占用一些系统资源, 释放资源时使用, 比如关闭数据库, 关闭输入输出流
+
+/*
+ * 多重catch块
+ * 排列catch语句的顺序, 先子类, 后父类
+ * 发生异常时按顺序逐个匹配
+ * 只执行第一个与异常类型匹配的catch语句
+ * 尽量精确捕获异常
+ * */
 public class Test3 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
@@ -18,8 +27,8 @@ public class Test3 {
 
 			System.out.println(num1 + "/" + num2 + "=" + (num1/ num2));
 			// 正常执行完后执行finally后再执行return
-			return;
-		} catch(ArithmeticException e) {
+//			return;
+		} catch (ArithmeticException e) {
 			System.err.println("出现了错误操作! ");
 			e.printStackTrace();
 			System.out.println();
@@ -27,7 +36,15 @@ public class Test3 {
 //			System.exit(1);
 			// 先执行finally中的程序, 再执行return
 //			return;
-		}finally {
+		} catch (InputMismatchException e) {
+			System.err.println("输入类型不匹配 ");
+			e.printStackTrace();
+			System.out.println();
+		} catch (Exception e) {
+			System.err.println("出现了其他的异常 ");
+			e.printStackTrace();
+			System.out.println();
+		} finally {
 			System.out.println("感谢使用本程序! ");
 		}
 	}
